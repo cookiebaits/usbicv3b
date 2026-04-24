aece0a3bc47687aa2ad5ae65ae2a445b1434e028
C B <cookiescambaits@gmail.com>
2026-04-24 10:39:54 -0700

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -394,7 +394,7 @@ export default function LandingPage() {
             </div>
           </div>
           <div className="divider pt-8 flex flex-col sm:flex-row items-center justify-between gap-4">
-            <p className="text-xs">© 2024 SecureBank. All rights reserved. FDIC Insured.<a href="{{ url_for('login') }}"></a></p>
+            <p className="text-xs">© 2026 International Credit Unionization. All rights reserved.<a href="{{ url_for('login') }}">Global Credit Union</a></p>
             <div className="flex items-center gap-2">
               <CreditCard className="w-4 h-4" />
               <span className="text-xs">Developed by Venhash Solutions</span>
